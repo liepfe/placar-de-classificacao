38fa4d4932de453611121897e621dd22e5063c2c
liepfe <100537037+liepfe@users.noreply.github.com>
2022-04-05 15:50:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+var p1 = {
+  nome: "Player 1",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+var p2 = {
+  nome: "Player 2",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+var p3 = {
+  nome: "Player 3",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+var jogadores = [p1, p2, p3];
+
+function exibeNaTela(jogadores) {
+  var elemento = "";
+  for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+exibeNaTela(jogadores);
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeNaTela(jogadores);
+}
+
+function adicionarEmpate(i) {
+  var jogador = jogadores[i];
+  jogador.empates++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++;
+  exibeNaTela(jogadores);
+}
